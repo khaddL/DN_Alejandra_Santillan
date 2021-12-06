d59d6a3f69db738deb8139d7dc85ec5e337eeae1
Ale <aly.santill.1203@gmail.com>
2021-12-06 12:39:56 -0600

    ejercicio 3

---
+++ Tema2-WebDevelopment/Ejercicios/E3/Datatypes/DataTypes.web/wwwroot/dataTypes.js
@@ -0,0 +1,51 @@
+﻿
+var hoy = new Date();
+var fecha = hoy.getDate() + '-' + (hoy.getMonth() + 1) + '-' + hoy.getFullYear();
+
+var myArray = [
+    {
+        "name": "Fernando",
+        "date": fecha,
+       
+    },
+    {
+        "name": "Rodolfo",
+        "date": fecha,
+    },
+    {
+        "name": "Alejandro",
+        "date": fecha,
+    },
+    {
+        "name": "Maria",
+        "date": fecha,
+    },
+    {
+        "name": "Rogelio",
+        "date": fecha,
+
+    },
+    {
+        "name": "Carmina",
+        "date": fecha,
+    },
+    {
+        "name": "Maria",
+        "date": fecha,
+    },
+    {
+        "name": "Cesar",
+        "date": fecha,
+    },
+    {
+        "name": "Nina",
+        "date": fecha,
+    },
+    {
+        "name": "Neska",
+        "date": fecha,
+    }]
+var foo = myArray.map(function (bar) {
+    return '<li>' + bar.name + ' ' + bar.date + '</li>';
+})
+document.getElementById("foo").innerHTML = foo;
---
+++ Tema2-WebDevelopment/Ejercicios/E3/Datatypes/Datatypes/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Datatypes
+{
+
+
+
+    public class Example
+    {
+
+        public static void Main(String[] args  )
+        {
+
+           
+            string Date = DateTime.Now.ToString("dd-MM-yyyy");
+
+            List<String> name = new List<String>();
+            name.Add("Fernando");
+            name.Add("Rodolfo");
+            name.Add("Alejandro");
+            name.Add("Maria");
+            name.Add("Rogelio");
+            name.Add("Carmina");
+            name.Add("Maria");
+            name.Add("Nina");
+            name.Add("Cesar");
+            name.Add("Neska");
+
+
+           
+
+            for (int i = 0; i < name.Count; i++)
+            {
+                Console.WriteLine("Element#{0} = {1}", i, name[i] + "   " + Date);
+            }
+
+            Console.WriteLine("        ");
+
+            foreach (string cosa in name) {
+
+                Console.WriteLine("Element = " + cosa + "    " + Date) ;   
+            
+            }   
+
+                Console.ReadKey();
+
+                Console.WriteLine("        ");
+
+            int a = 0;
+            while (a <= name.Count) {
+
+                Console.WriteLine("Element = " + name[a] + "    " + Date);
+                  a++;
+            }
+
+            Console.ReadKey();
+
+            Console.WriteLine("        ");
+
+
+            
+            do
+            {
+                Console.WriteLine("Element = " + name[a] + "    " + Date);
+                a++;
+            }
+            while (a <= name.Count);
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
