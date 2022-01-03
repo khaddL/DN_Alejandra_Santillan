d5cb0912f42c5464d0deed66660370351949a9f5
Ale <aly.santill.1203@gmail.com>
2022-01-02 18:25:41 -0600

    cambios

---
+++ Tema2-WebDevelopment/Ejercicios/E4/DN_2.4.4_Alejandra_Santillan/GymManager.Web/GymManager.Web/wwwroot/JavaScript.js
@@ -0,0 +1,43 @@
+﻿
+
+var button = document.getElementById("example");
+
+var textbox = document.getElementById("examples");
+
+
+const Keyenter = 13;
+textbox.addEventListener("keypress", function (e) {
+
+    if (e.keyCode == Keyenter) {
+
+        ModalTest();
+    }
+});
+
+
+
+
+button.addEventListener("click", function (event) {
+
+    ModalTest();
+
+
+});
+
+
+
+
+function ModalTest() {
+    let examples = document.getElementById("examples").value;
+    document.getElementById("valueInput").innerHTML = "Check-out for " + examples + " " + moment(new Date()).format("DD-MM-YYYY h:mm:ss a");
+
+    var myModal = new bootstrap.Modal(document.getElementById('contenido-modal'), {
+        keyboard: false
+        
+
+    })
+    
+    
+    myModal.show();
+    
+}
